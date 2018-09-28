fec926f068bb67985a58fc675315a985c87292a8
Dmitry Tarantin <dimichspb@gmail.com>
2018-09-28 11:56:34 +0300

    Init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=triget',
+    'username' => 'triget',
+    'password' => 'triget',
+    'charset' => 'utf8',
+];
